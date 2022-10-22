a6edef56c2a64041489abe199bdd369411e70026
Tomasz-Budny <s208246@sggw.edu.pl>
2022-10-22 18:38:39 +0200

    Add Singleton design pattern

---
+++ Singleton/DataBase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Singleton
+{
+    // Singleton use case example
+    public class DataBase
+    {
+        private DataBase() {}
+        private static DataBase _dataBase;
+
+        public static DataBase GetInstance()
+        {
+            if (_dataBase == null)
+            {
+                _dataBase = new DataBase();
+            }
+
+            return _dataBase;
+        }
+
+        public void SomeBusinessLogic()
+        {
+            Console.WriteLine("Some logic");
+        }
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataBase dataBase = DataBase.GetInstance();
+            dataBase.SomeBusinessLogic();
+        }
+    }
+}
