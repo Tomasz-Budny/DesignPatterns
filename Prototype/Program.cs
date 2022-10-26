9b7332c0011717d45b153952b383e7b84914a2b4
Tomasz-Budny <s208246@sggw.edu.pl>
2022-10-26 19:02:16 +0200

    Add prototype design pattern

---
+++ Prototype/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Prototype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Server server1 = new Server()
+            {
+                Name = "Server #1",
+                Processor = new CPU()
+                {
+                    Name = "CPU #1",
+                    Speed = 21
+                }
+            };
+
+            var server2 = server1.ShallowClone();
+            var server3 = server1.DeepClone();
+
+            Console.WriteLine("before modifying server1:");
+            Console.WriteLine("Server 1: " + server1);
+            Console.WriteLine("Server 2: " + server2);
+            Console.WriteLine("Server 3: " + server3);
+
+            server1.Name = "different Name";
+            server1.Processor.Name = "different processor";
+            
+            Console.WriteLine("\nafter modifying server1:");
+            Console.WriteLine("Server 1: " + server1);
+            Console.WriteLine("Server 2: " + server2);
+            Console.WriteLine("Server 3: " + server3);
+        }
+    }
+}
---
+++ Prototype/Server.cs
@@ -0,0 +1,36 @@
+﻿namespace Prototype
+{
+    public class Server
+    {
+        public string Name { get; set; }
+        public CPU Processor { get; set; }
+
+        public Server ShallowClone()
+        {
+            return (Server)MemberwiseClone();
+        }
+
+        public Server DeepClone()
+        {
+            Server clone = (Server)MemberwiseClone();
+            clone.Processor = new CPU()
+            {
+                Name = clone.Processor.Name,
+                Speed = clone.Processor.Speed
+            };
+            return clone;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}\nCPU: Name: {Processor.Name}, Speed: {Processor.Speed}]";
+        }
+
+    }
+
+    public class CPU
+    {
+        public string Name { get; set; }
+        public float Speed { get; set; }
+    }
+}
