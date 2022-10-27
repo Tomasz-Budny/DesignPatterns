c122d724ab4b1b6fd51cd2eab11e9af447cf95f4
Tomasz-Budny <s208246@sggw.edu.pl>
2022-10-27 14:30:48 +0200

    Add composite design pattern

---
+++ Composite/ComplexOrder.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+
+namespace Composite
+{
+    public interface IValuable
+    {
+        int CalculatePrice();
+    }
+    
+    public class ConcreteProduct : IValuable
+    {
+        private readonly string _name;
+        private readonly int _price;
+
+        public ConcreteProduct(int price, string name)
+        {
+            _name = name;
+            this._price = price;
+        }
+
+        public int CalculatePrice()
+        {
+            return _price;
+        }
+    }
+    
+    public class Order : IValuable
+    {
+        private readonly List<IValuable> _products = new List<IValuable>();
+
+        public void Add(IValuable product)
+        {
+            _products.Add(product);
+        }
+        
+        public int CalculatePrice()
+        {
+            int total = 0;
+            foreach (var product in _products)
+            {
+                total += product.CalculatePrice();
+            }
+            return total;
+        }
+    }
+}
---
+++ Composite/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Composite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order();
+            Order subOrder = new Order();
+            subOrder.Add(new ConcreteProduct(100, "product A"));
+            subOrder.Add(new ConcreteProduct(100, "product B"));
+            Order subSubOrder = new Order();
+            subSubOrder.Add(new ConcreteProduct(50, "product C"));
+            subOrder.Add(subSubOrder);
+            
+            order.Add(subOrder);
+            order.Add(new ConcreteProduct(50, "product D"));
+
+            Console.WriteLine(order.CalculatePrice());
+        }
+    }
+}
