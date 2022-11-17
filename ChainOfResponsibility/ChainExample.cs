cd829c84fdd756cf5672d3cb027fa88f08827ff1
Tomasz-Budny <s208246@sggw.edu.pl>
2022-11-17 13:36:57 +0100

    Add Chain of responsibility design pattern

---
+++ ChainOfResponsibility/ChainExample.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Linq;
+
+namespace ChainOfResponsibility
+{
+    public interface IPasswordCheckerHandler
+    {
+        bool IsValid(string password);
+    }
+    
+    public abstract class PasswordCheckerHandler : IPasswordCheckerHandler
+    {
+        public IPasswordCheckerHandler NextHandler { get; set; }
+
+        public abstract bool IsValid(string password);
+
+        protected void ErrorMessage(string message)
+        {
+            Console.WriteLine($"Password is invalid: {message}");
+        }
+    }
+
+    public class PasswordIsLongerThan : PasswordCheckerHandler
+    {
+        private int minLength;
+        
+        public PasswordIsLongerThan(int minLength)
+        {
+            this.minLength = minLength;
+        }
+        
+        public override bool IsValid(string password)
+        {
+            if (password.Length >= minLength)
+            {
+                if (NextHandler != null)
+                    return NextHandler.IsValid(password);
+                return true;
+            }
+
+            ErrorMessage($"Password is shorter than {minLength}");
+            return false;
+        }
+    }
+
+    public class PasswordHaveUpperAndLowerCase : PasswordCheckerHandler
+    {
+        public override bool IsValid(string password)
+        {
+            if (password.ToUpper() != password)
+            {
+                if (NextHandler != null)
+                    return NextHandler.IsValid(password);
+                return true;
+            }
+
+            ErrorMessage("Password doesnt contains upper and lower cases");
+            return false;
+        }
+    }
+
+    public class PasswordContainsNumbers : PasswordCheckerHandler
+    {
+        public override bool IsValid(string password)
+        {
+            if (IsContainsNumbers(password))
+            {
+                if (NextHandler != null)
+                    return NextHandler.IsValid(password);
+                return true;
+            }
+
+            ErrorMessage($"Password doesnt contains any number!");
+            return false;
+        }
+
+        private bool IsContainsNumbers(string password)
+        {
+            foreach (var c in password)
+            {
+                if (c >= '0' && c <= '9')
+                    return true;
+            }
+
+            return false;
+        }
+    }
+    
+    public class PasswordContainsSpecialCharacter : PasswordCheckerHandler
+    {
+        public override bool IsValid(string password)
+        {
+            if (password.Any(c => char.IsSymbol(c) || char.IsControl(c)))
+            {
+                if (NextHandler != null)
+                    return NextHandler.IsValid(password);
+                return true;
+            }
+
+            ErrorMessage($"Password doesnt contains any special characters");
+            return false;
+        }
+    }
+}
---
+++ ChainOfResponsibility/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ChainOfResponsibility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PasswordIsLongerThan handler1 = new PasswordIsLongerThan(9);
+            PasswordHaveUpperAndLowerCase handler2 = new PasswordHaveUpperAndLowerCase();
+            PasswordContainsNumbers handler3 = new PasswordContainsNumbers();
+            PasswordContainsSpecialCharacter handler4 = new PasswordContainsSpecialCharacter();
+
+            handler1.NextHandler = handler2;
+            handler2.NextHandler = handler3;
+            handler3.NextHandler = handler4;
+
+            var password = "HasłoHasło+";
+            var result = handler1.IsValid(password);
+            Console.WriteLine(result);
+        }
+    }
+}
