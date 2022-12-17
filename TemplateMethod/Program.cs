441d1dcafcfe3233af29eaed6acb48e46e131a7d
Tomasz-Budny <s208246@sggw.edu.pl>
2022-12-17 10:46:23 +0100

    Add Template Method design pattern

---
+++ TemplateMethod/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TemplateMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var r1 = new RoombaVacuumRobot();
+            r1.Vacuum();
+
+            Console.WriteLine();
+            
+            var r2 = new ChineseVacuumRobot();
+            r2.Vacuum();
+        }
+    }
+}
---
+++ TemplateMethod/TemplateMethodExample.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace TemplateMethod
+{
+    public abstract class VacuumRobot
+    {
+        public void Vacuum()
+        {
+            Move();
+            TurnLeft();
+            TurnRight();
+            TurnOnLight();
+            SoundSignal();
+            UseMop();
+        }
+
+        protected abstract void Move();
+        protected abstract void TurnLeft();
+        protected abstract void TurnRight();
+        protected abstract void TurnOnLight();
+        protected abstract void SoundSignal();
+        protected abstract void UseMop();
+    }
+    
+    public class RoombaVacuumRobot: VacuumRobot
+    {
+        protected override void Move()
+        {
+            Console.WriteLine("Moving forward");
+        }
+
+        protected override void TurnLeft()
+        {
+            Console.WriteLine("Turning left");
+        }
+
+        protected override void TurnRight()
+        {
+            Console.WriteLine("Turning Right");
+        }
+
+        protected override void TurnOnLight()
+        {
+            Console.WriteLine("Turning lights on");
+        }
+
+        protected override void SoundSignal()
+        {
+            Console.WriteLine("Beep beep");
+        }
+
+        protected override void UseMop()
+        {
+            Console.WriteLine("Using mop");
+        }
+    }
+
+    public class ChineseVacuumRobot: VacuumRobot
+    {
+        protected override void Move()
+        {
+            Console.WriteLine("Move forward 1 meter");
+        }
+
+        protected override void TurnLeft()
+        {
+            Console.WriteLine("Turning left");
+        }
+
+        protected override void TurnRight()
+        {
+            Console.WriteLine("Turning Right");
+        }
+
+        protected override void TurnOnLight()
+        {
+            // dont have lights
+        }
+
+        protected override void SoundSignal()
+        {
+            Console.WriteLine("Beep beep");
+        }
+
+        protected override void UseMop()
+        {
+            Console.WriteLine("Using mop");
+        }
+    }
+}
