362772f7745d40bfe26b085664228272ea2dedfb
Tomasz-Budny <s208246@sggw.edu.pl>
2022-12-25 19:30:36 +0100

    Add memento design pattern

---
+++ Memento/Notepad.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Memento
+{
+    public class Memento
+    {
+        public string TextLine { get; }
+
+        public Memento(string textLine)
+        {
+            TextLine = textLine;
+        }
+    }
+    
+    public class Notepad
+    {
+        public string TextLine { get; set; }
+        
+        public Memento Save()
+        {
+            var textLine = Console.ReadLine();
+            TextLine = textLine;
+            return new Memento(textLine);
+        }
+    }
+
+    public class NotepadCaretaker
+    {
+        private readonly Notepad _notepad;
+        private readonly List<Memento> _history = new List<Memento>();
+        
+        public NotepadCaretaker(Notepad notepad)
+        {
+            _notepad = notepad;
+        }
+
+        public void Save()
+        {
+            Console.Write("Your Text goes here: ");
+            var memento = _notepad.Save(); 
+            _history.Add(memento); 
+        }
+
+        public void Undo()
+        {
+            if (_history.Count > 0)
+            {
+                var lastLine = _history[^1].TextLine;
+                _history.RemoveAt(_history.Count - 1);
+                _notepad.TextLine = lastLine;
+                Console.Write("Your last line is: ");
+                Console.WriteLine(lastLine);
+            }
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("Your all saved lines: ");
+            foreach (var memento in _history)
+            {
+                Console.WriteLine(memento.TextLine);
+            }
+        }
+    }
+}
---
+++ Memento/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Memento
+{
+
+    public class NotepadManager
+    {
+        private readonly NotepadCaretaker _caretaker = new NotepadCaretaker(new Notepad());
+        
+        public void Run()
+        {
+            var isRunning = true;
+            while (isRunning)
+            {
+                var btnPressed = Console.ReadLine();
+                switch (btnPressed)
+                {
+                    case "s":
+                        _caretaker.Save();
+                        break;
+                    case "u":
+                        _caretaker.Undo();
+                        break;
+                    case "a":
+                        _caretaker.Show();
+                        break;
+                    case "e":
+                        isRunning = false;
+                        break;
+                }
+            }
+        }
+
+        public void DisplayInstruction()
+        {
+            Console.WriteLine("s - insert one line to memery");
+            Console.WriteLine("u - delete last line and display it");
+            Console.WriteLine("a - display all saved lines");
+            Console.WriteLine("e - exit program");
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var program = new NotepadManager();
+            program.DisplayInstruction();
+            program.Run();
+        }
+    }
+}
